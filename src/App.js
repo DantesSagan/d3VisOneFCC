2c1b9b223940812be30343c352806e591cc77793
lexsander ikhaylov <mihaleksval@gmail.com>
2021-08-27 17:48:04 +0500

    almost all tests done

--- src/App.js
+++ src/App.js
@@ -1,58 +1,144 @@
+import React, { useState } from 'react';
 import * as d3 from 'd3';
-import { useRef, useState, useEffect } from 'react';
-
-import axios from 'axios';
-
-import XAxis from './components/xAxis';
-import './App.css';
 
 export default function App() {
-  const [dataset, setDataset] = useState([12, 31, 22, 17, 25, 18, 29, 14, 9]);
-  const [posts, setPosts] = useState({});
-  const ref = useRef();
+  const [url] = useState(
+    'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json'
+  );
+  const [req] = useState(new XMLHttpRequest());
+  let [data] = useState();
+  let [values] = useState();
 
-  const w = 500;
-  const h = 100;
+  let [xScale] = useState();
 
-  useEffect(() => {
-    axios
-      .get(
-        'https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json'
-      )
-      .then((res) => {
-        console.log(res);
-        setPosts(res.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-      
-    const svgElement = d3
+  let heightScale;
+
+  let xAxisScale;
+  let yAxisScale;
+
+  const width = 800;
+  const height = 600;
+  const padding = 60;
+
+  let svg = d3.select('svg');
+
+  const drawCanvas = () => {
+    svg.attr('width', width);
+    svg.attr('height', height);
+  };
+  const generateScales = () => {
+    heightScale = d3
+      .scaleLinear()
+      .domain([
+        0,
+        d3.max(values, (item) => {
+          return item[1];
+        }),
+      ])
+      .range([0, height - 2 * padding]);
+
+    xScale = d3
+      .scaleLinear()
+      .domain([0, values.length - 1])
+      .range([padding, width - padding]);
+
+    const datesArray = values.map((item) => {
+      return new Date(item[0]);
+    });
+    console.log(datesArray);
+
+    xAxisScale = d3
+      .scaleTime()
+      .domain([d3.min(datesArray), d3.max(datesArray)])
+      .range([padding, width - padding]);
+
+    yAxisScale = d3
+      .scaleLinear()
+      .domain([
+        0,
+        d3.max(values, (item) => {
+          return item[1];
+        }),
+      ])
+      .range([height - padding, padding]);
+
+    return { xScale, datesArray };
+  };
+  const drawBars = () => {
+    const tooltip = d3
       .select('body')
-      .append('svg')
-      .attr('width', w)
-      .attr('height', h);
+      .append('div')
+      .attr('id', 'tooltip')
+      .style('visibility', 'hidden')
+      .style('width', 'auto')
+      .style('height', 'auto');
 
-    svgElement
+    svg
       .selectAll('rect')
-      .data(dataset)
+      .data(values)
       .enter()
       .append('rect')
-      .attr('x', (d, i) => i * 30)
-      .attr('y', (d, i) => {
-        // Add your code below this line
-        return h - 3 * d;
-
-        // Add your code above this line
+      .attr('class', 'bar')
+      .attr('width', (width - 2 * padding) / values.length)
+      .attr('data-date', (item) => {
+        return item[0];
       })
-      .attr('width', 25)
-      .attr('height', (d, i) => 3 * d);
-  }, [dataset]);
+      .attr('data-gdp', (item) => {
+        return item[1];
+      })
+      .attr('height', (item) => {
+        return heightScale(item[1]);
+      })
+      .attr('x', (item, i) => {
+        return xScale(i);
+      })
+      .attr('y', (item) => {
+        return height - padding - heightScale(item[1]);
+      })
+      .on('mouseover', (item) => {
+        tooltip.transition().duration(200).style('visibility', 'visible');
+        tooltip.text(item[0], item[1]);
 
+        document.querySelector('#tooltip').setAttribute('data-date', item[0]);
+      })
+      .on('mouseout', () => {
+        tooltip.transition().duration(200).style('visibility', 'hidden');
+      });
+  };
+
+  const generateAxis = () => {
+    const xAxis = d3.axisBottom(xAxisScale);
+    const yAxis = d3.axisLeft(yAxisScale);
+    svg
+      .append('g')
+      .call(xAxis)
+      .attr('id', 'x-axis')
+      .attr('transform', 'translate(0, ' + (height - padding) + ')');
+
+    svg
+      .append('g')
+      .call(yAxis)
+      .attr('id', 'y-axis')
+      .attr('transform', 'translate(' + padding + ',  0)');
+    return { xAxis, svg, yAxis };
+  };
+
+  req.open('GET', url, true);
+  req.onload = () => {
+    data = JSON.parse(req.responseText);
+    values = data.data;
+    console.log(values);
+    drawCanvas();
+    generateScales();
+    drawBars();
+    generateAxis();
+  };
+  req.send();
   return (
-    <div className='mx-auto rounded-xl p-3 mt-44 '>
-      <svg ref={ref} />
-      <XAxis />
-    </div>
+    <svg>
+      <text id='title' x='360' y='30'>
+        USA GDP
+      </text>
+    </svg>
   );
 }
